ba6b12f21668ff5c257fb69676a23f153883c202
abarnasri <36784030+abarnasri@users.noreply.github.com>
2019-11-20 02:08:50 -0500

    Update event_pipeline.cpp

--- event_pipeline.cpp
+++ event_pipeline.cpp
@@ -1,10 +1,16 @@
+/* Class for event pipeline
+ * CREATED BY: Lathan Thangavadivel
+ * LAST EDITED BY: Abarna Srikantharajah
+ * LAST EDITED: 20/11/2019
+ * TODO:
+ */
 
 
 #include <queue>
 
 using namespace std;
 
-
+Event_Pipeline::Event_Pipeline(){}
 
 bool CompareTime (Event const & event1, Event const & event2) {
         double diff = std::difftime(std::event1.get_time(),std::event2..get_time());
