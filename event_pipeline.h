e2100e2542f3246a99bbd4c63e952b8314515503
lathant <47580015+lathant@users.noreply.github.com>
2019-11-20 02:01:42 -0500

    Add files via upload

---
+++ event_pipeline.h
@@ -0,0 +1,12 @@
+#ifndef EVENT_PIPELINE_H
+#define EVENT_PIPELINE_H
+
+#include "Event.h"
+#include <queue>
+
+
+class event_pipeline{
+    private:
+            std::queue<event,vector<event>,CompareTime>;
+            struct
+}
