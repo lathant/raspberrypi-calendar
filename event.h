58ab30358a14e53326f0fe0b29c5fc4e81a4f486
lathant <47580015+lathant@users.noreply.github.com>
2019-11-22 06:23:14 -0500

    Update event.h

--- event.h
+++ event.h
@@ -15,23 +15,26 @@ class Event {
 private:
     std::string eventName;
     std::string details;
-    std::time_t time;
     std::string access_t;
     std::string owner_id;
     std::string repeat_type;
+    std::time_t start_time;
+    std::time_t end_time;
     
     
 public:
-    Event(std::string in_eventName, std::string in_details, std::time_t in_time,std::string in_access_t, std::string in_owner_id, std::string in_repeatType);
+    Event(std::string in_eventName, std::string in_details, std::time_t in_start_time,std::in_end_time,std::string in_access_t, std::string in_owner_id, std::string in_repeatType);
     std::string get_eventName(){return eventName;}
     std::string get_details(){return details;}
-    std::time_t get_time(){return time;}
+    std::time_t get_start_time(){return start_time;}
+    std::time_t get_end_time(){return end_time;}
     std::string get_access_t(){return access_t;}
     std::string get_owner_id(){return owner_id;}
     std::string get_repeat_type(){return repeat_type;}
     void set_eventName(std::string newEventName);
     void set_details(std::string newDetails);
-    void set_time(std::string newTime);
+    void set_start_time(std::time_t newStartTime);
+    void set_end_time(std::time_t newEndTime);
     void set_access_t(std::string newAccess);
     void set_owner_id(std::string newOwner);
     void set_repeat_type(std::string newRepeat);
