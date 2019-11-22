c91ca3aa621b4e81baa373632ed63710ff3a5999
lathant <47580015+lathant@users.noreply.github.com>
2019-11-22 06:26:48 -0500

    Update event_factory.cpp

--- event_factory.cpp
+++ event_factory.cpp
@@ -12,7 +12,7 @@ using namespace std;
 //implement factory method
 Event_Factory::Event_Factory(){};
 
-Event* Event_Factory::create_event(string eventName, string details, time_t time,string access_t, string owner_id, string repeatType) {
-    Event* event = new Event(string eventName, string details, time_t time,string access_t, string owner_id, string repeatType);
+Event* Event_Factory::create_event(string eventName, string details, time_t start_time,time_t end_time,string access_t, string owner_id, string repeatType) {
+    Event* event = new Event(string eventName, string details, time_t start_time,time_t end_time,string access_t, string owner_id, string repeatType);
     return event;
 }
