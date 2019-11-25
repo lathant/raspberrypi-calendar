154a66b4725cad4d559930dd5173a45062a0f209
lathant <47580015+lathant@users.noreply.github.com>
2019-11-25 03:29:43 -0500

    Update reminder.cpp

--- reminder.cpp
+++ reminder.cpp
@@ -2,20 +2,14 @@
 
 using namespace std;
 
-Reminder::Reminder(string in_event, string in_destination, time_t in_remind_time){
-    event = in_event
-    destination = in_destination;
-    remind_time = in_remind_time
-}
-
 void Reminder::set_event(string newEvent){
-    event = newEvent;
+    Reminder::event = newEvent;
 }
 
-void Event::set_destination(string newDestination){
+void Reminder::set_destination(string newDestination){
     destination = newDestination;
 }
 
-void Event::set_remind_time(time_t newRemindTime){
+void Reminder::set_remind_time(time_t newRemindTime){
     remind_time = newRemindTime;
 }
