c865e91729c712930f7f49a10c555d80c4412f8e
lathant <47580015+lathant@users.noreply.github.com>
2019-11-27 02:40:33 -0500

    Update event_manager.cpp

--- event_manager.cpp
+++ event_manager.cpp
@@ -7,6 +7,8 @@
  #include <fstream>
  #include <string.h>
  #include <stdio.h>
+ #include <vector>
+ #include <sstream>
  #include "string"
  #include "event_manager.h"
 
@@ -32,7 +34,46 @@ Event_Manager* Event_Manager::get_instance() {
  *
  */
 Event* Event_Manager::get_event(string event_name){
-
+     // File pointer 
+    fstream fin; 
+  
+    // Open an existing file 
+    fin.open(STORAGE_FILE_PATH, ios::in); 
+  
+    // Read the Data from the file 
+    // as String Vector 
+    vector<string> row; 
+    string line, word, temp,token; 
+    string delimiter = "^@^";
+    size_t pos;
+    
+    while (fin >> temp) {
+  
+        row.clear(); 
+  
+        // read an entire row and 
+        // store it in a string variable 'line' 
+        getline(fin, line); 
+        
+        pos = 0;
+        
+        while ((pos = line.find(delimiter)) != string::npos) {
+            token = line.substr(0, pos);
+            row.push_back(token);
+            line.erase(0, pos + delimiter.length());
+        }
+        
+        if (row[0].compare(event_name) == 0){
+            Event_Factory* factory = new Event_Factory();
+            Event* event = factory->create_event(row[0], row[1], stoi(row[2]), stoi(row[3]),
+                    row[4], row[5], row[6]);
+            return event;
+            
+        }
+        
+    return NULL;
+        
+    }
 }
 
 /**
@@ -49,7 +90,7 @@ Event* Event_Manager::get_event(string event_name){
  * @param       repeatType
  * @return      int                 -1 duplicate, 0 on failure, and 1 on success
  */
-int Event_Manager::create_event(string eventName, string details, time_t start_time,
+int Event_Manager::create_event (string eventName, string details, time_t start_time,
     time_t end_time, string access_t, string owner_id, string repeatType){
     Event* event = get_event(eventName);
     if (event != NULL)
@@ -57,8 +98,8 @@ int Event_Manager::create_event(string eventName, string details, time_t start_t
     Event_Factory* factory = new Event_Factory();
     event = factory->create_event(eventName, details, start_time, end_time,
         access_t, owner_id, repeatType);
-    string event_db_entry = eventName + "," + details + "," + to_string(start_time) +
-        "," + to_string(end_time) + "," + access_t + "," + owner_id + "," + repeatType;
+    string event_db_entry = eventName + "^@^" + details + "^@^" + to_string(start_time) +
+        "^@^" + to_string(end_time) + "^@^" + access_t + "^@^" + owner_id + "^@^" + repeatType;
     ofstream out(STORAGE_FILE_PATH, ios::app);
     out << event_db_entry << endl;
     out.close();
@@ -80,8 +121,11 @@ int Event_Manager::delete_event(string event_name){
     ifstream file_input(STORAGE_FILE_PATH);
     string line, current_event_name;
     string new_database_string = "";
+    
+    size_t pos;
     while(getline(file_input, line)) {
-        current_event_name = ;
+        pos = line.find("^@^");
+        current_event_name = line.substr(0,pos);
 
         if (current_event_name.compare(event_name) != 0) {
             line = line + "\n";
@@ -105,8 +149,45 @@ int Event_Manager::delete_event(string event_name){
  * @param       owner_id          The name of the user
  * @return
  */
-set<Event> Event_Manager::get_personal_events(string owner_id){
+vector<Event> Event_Manager::get_personal_events(string owner_id){
     // read file line by line and create event objects feed all event objects where owner matches into set
+    
+    // File pointer 
+    fstream fin; 
+    vector<Event> output;
+    // Open an existing file 
+    fin.open(STORAGE_FILE_PATH, ios::in); 
+  
+    // Read the Data from the file 
+    // as String Vector 
+    vector<string> row; 
+    string line, word, temp,token; 
+    string delimiter = "^@^";
+    size_t pos;
+    
+    while (fin >> temp) {
+
+        row.clear(); 
+
+        // read an entire row and 
+        // store it in a string variable 'line' 
+        getline(fin, line); 
+        
+        pos = 0;
+        
+        while ((pos = line.find(delimiter)) != string::npos) {
+            token = line.substr(0, pos);
+            row.push_back(token);
+            line.erase(0, pos + delimiter.length());
+        }
+        if (row[5].compare(owner_id) == 0){
+            Event_Factory* factory = new Event_Factory();
+            Event* newEvent = factory->create_event(row[0], row[1], stoi(row[2]), stoi(row[3]),
+                                                    row[4], row[5], row[6]);
+            output.push_back(*newEvent);
+        }
+    }
+    return output;
 }
 
 /**
@@ -116,8 +197,43 @@ set<Event> Event_Manager::get_personal_events(string owner_id){
  * @date        25/11/2019
  * @return
  */
-set<Event> Event_Manager::get_public_events(){
+vector<Event> Event_Manager::get_public_events(){
+    // File pointer 
+    fstream fin; 
+    vector<Event> output;
+    // Open an existing file 
+    fin.open(STORAGE_FILE_PATH, ios::in); 
+  
+    // Read the Data from the file 
+    // as String Vector 
+    vector<string> row; 
+    string line, word, temp,token; 
+    string delimiter = "^@^";
+    size_t pos;
+    
+    while (fin >> temp) {
 
+        row.clear(); 
+
+        // read an entire row and 
+        // store it in a string variable 'line' 
+        getline(fin, line); 
+        
+        pos = 0;
+        
+        while ((pos = line.find(delimiter)) != string::npos) {
+            token = line.substr(0, pos);
+            row.push_back(token);
+            line.erase(0, pos + delimiter.length());
+        }
+        if (row[6].compare("public") == 0){
+            Event_Factory* factory = new Event_Factory();
+            Event* newEvent = factory->create_event(row[0], row[1], stoi(row[2]), stoi(row[3]),
+                                                    row[4], row[5], row[6]);
+            output.push_back(*newEvent);
+        }
+    }
+    return output;
 }
 
 /**
