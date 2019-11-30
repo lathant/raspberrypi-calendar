a9ba3929baf9adfd3f2799e601896cc89f5dca7d
vzhurov3 <vzhurov3@uwo.ca>
2019-11-30 01:49:51 -0500

    Refactor + Fix

--- event_manager.cpp
+++ event_manager.cpp
@@ -1,11 +1,10 @@
-/* Class for the event manager
- * CREATED BY: Lathan Thangavadivel
- * LAST EDITED BY: Lathan Thangavadivel
- * LAST EDITED: 25/11/2019
- * @brief the file that contains the functions to handle events
+/**
+ * @brief implementation of event manager
  *
- * @author  Lathan Thangavadivel, Vladimir Zhurov
- * @date   25/11/2019
+ * The implementation of methods that manipulatet the Event storage file
+ * @author  Lathan Thangavadivel
+ * @author  Vladimir Zhurov
+ * @date    30/11/2019
  */
 
 #include <iostream>
@@ -23,87 +22,59 @@ using namespace std;
 static string STORAGE_FILE_PATH = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation).toStdString() + "/events.csv";
 
 Event_Manager* Event_Manager::instance = NULL;
+Event_Factory* factory;
 /**
- * @brief       returns an instance of Event_Manager
+ * @brief   returns an instance of Event_Manager
  *
  * returns an instance of Event_Manager and enforces singleton design
- * @Author      Vladimir Zhurov
- * @date        25/11/2019
+ * @Author  Vladimir Zhurov
+ * @date    30/11/2019
  */
 Event_Manager* Event_Manager::get_instance() {
-    if (instance == NULL)
+    if (instance == NULL){
         instance = new Event_Manager;
+        factory = new Event_Factory();
+    }
     return instance;
 }
 
-/*
-* @brief gets the event attribute string
-*
-* @author             Lathan Thangavadivel
-* @param  event_name  event name as a string
-* @return event      event attribute
-*/
+/**
+ * @brief gets the event attribute string
+ *
+ * @author   Lathan Thangavadivel
+ * @author   Vladimir Zhurov
+ * @date     30/11/2019
+ * @param    event_name      event name as a string
+ * @return   event           event attribute
+ */
 Event* Event_Manager::get_event(string event_name){
-     // File pointer
+    string line, name;
+    vector<string> parts;
 
-    ifstream fin (STORAGE_FILE_PATH.c_str());
+    ifstream event_file(STORAGE_FILE_PATH);
+    if(!event_file.is_open())
+        return NULL; // Error occoured
 
-    if (fin.good() == false) {
-        fin.close();
-        return NULL;
+    while(getline(event_file, line)){
+            // Parse string into componants
+            stringstream ss (line);
+            while(getline(ss, line, "^@^"))
+                parts.push_back(line);
+            // If event_name matches return Event
+            if (parts[0].compare(event_name) == 0){
+                return factory->create_event(parts[0], parts[1], stol(parts[2]), stol(parts[3]),
+                    parts[4], parts[5], parts[6]);
+            }
+            parts.clear(); // Clean up parts for next line
     }
-
-
-    // Open an existing file
-    fin.open(STORAGE_FILE_PATH, ios::in);
-
-    // Read the Data from the file
-    // as String Vector
-    vector<string> row;
-    string line, word, temp,token;
-    string delimiter = "^@^";
-    size_t pos;
-
-    while (fin >> temp) {
-
-        row.clear();
-
-        // read an entire row and
-        // store it in a string variable 'line'
-
-        getline(fin, line);
-
-        pos = 0;
-
-        while ((pos = line.find(delimiter)) != string::npos) {
-            token = line.substr(0, pos);
-            row.push_back(token);
-            line.erase(0, pos + delimiter.length());
-        }
-
-
-        if (row[0].compare(event_name) == 0){
-            Event_Factory* factory = new Event_Factory();
-            Event* event = factory->create_event(row[0], row[1], stoi(row[2]), stoi(row[3]), row[4], row[5], row[6]);
-            fin.close();
-            return event;
-
-        }
-        if (fin.eof()) {
-            fin.close();
-            return NULL;
-        }
-
-    }
-    fin.close();
-    return NULL;
+    return NULL; // Not found
 }
 
 /**
  * @brief       create a new event
  *
  * @author      Vladimir Zhurov
- * @date        25/11/2019
+ * @date        30/11/2019
  * @param       eventName
  * @param       details
  * @param       start_time
@@ -115,18 +86,18 @@ Event* Event_Manager::get_event(string event_name){
  */
 int Event_Manager::create_event (string eventName, string details, time_t start_time,
     time_t end_time, string access_t, string owner_id, string repeatType){
+    // Check if event is already in file
     Event* event = get_event(eventName);
-
     if (event != NULL) {
         return -1;
     }
-    Event_Factory* factory = new Event_Factory();
-    event = factory->create_event(eventName, details, start_time, end_time,
-        access_t, owner_id, repeatType);
+    delete event; // free memory
+    // Write string rep to file;
     string event_db_entry = eventName + "^@^" + details + "^@^" + to_string(start_time) +
         "^@^" + to_string(end_time) + "^@^" + access_t + "^@^" + owner_id + "^@^" + repeatType;
-
     ofstream out(STORAGE_FILE_PATH, ios::app);
+    if(!out.is_open())
+        return 0;
     out << event_db_entry << endl;
     out.close();
     return 1;
@@ -135,36 +106,34 @@ int Event_Manager::create_event (string eventName, string details, time_t start_
 /**
  * @brief       delete a event
  *
+ * removes the given event from the storage file
  * @author      Vladimir Zhurov
- * @date        25/11/2019
+ * @date        30/11/2019
  * @param       event_name          The name of the event
  * @return      int                 0 on failure, 1 on success
  */
 int Event_Manager::delete_event(string event_name){
-    Event* event = get_event(event_name);
-    if (event == NULL)
-        return 0;
-    ifstream file_input(STORAGE_FILE_PATH);
-    string line, current_event_name;
-    string new_database_string = "";
+    string line, name, new_database_string = "";
+    size_t length;
+    int success = 0;
 
-    size_t pos;
-    while(getline(file_input, line)) {
-        pos = line.find("^@^");
-        current_event_name = line.substr(0,pos);
-
-        if (current_event_name.compare(event_name) != 0) {
-            line = line + "\n";
-            new_database_string += line;
-        }
+    ifstream event_file(STORAGE_FILE_PATH);
+    if(!event_file.is_open())
+        return success; // Error occoured
+    while(getline(event_file, line)){
+        length = line.find("^@^"); // Length of event_name
+        name = line.substr(0, length);
+        if(name.compare(event_name) != 0)
+            new_database_string += line + "\n"; // Write non matching event to string
+        else
+            success = 1;
     }
-    file_input.close();
-    remove(STORAGE_FILE_PATH.c_str());
+    event_file.close();
+    remove(STORAGE_FILE_PATH.c_str()); // Remove file (deletes matching events)
     ofstream out(STORAGE_FILE_PATH);
-    out << new_database_string;
+    out << new_database_string; // Make new file and write all non matching events to it
     out.close();
-
-    return 1;
+    return success;
 }
 
 /**
@@ -181,21 +150,22 @@ vector<Event> Event_Manager::get_personal_events(string owner_id){
     vector<Event> output;
     vector<string> parts;
     string line;
-    ifstream event_file;
-    event_file.open(STORAGE_FILE_PATH);
+
+    ifstream event_file(STORAGE_FILE_PATH);
+    if(!event_file.is_open())
+        return output; // Error occoured
     while(getline(event_file, line)){
         // Parse string into componants
         stringstream ss (line);
         while(getline(ss, line, "^@^"))
             parts.push_back(line);
-        // if owner_id matches add to collection
+        // If owner_id matches add to collection
         if (parts[5].compare(owner_id) == 0){
-            Event_Factory* factory = new Event_Factory();
             Event* newEvent = factory->create_event(parts[0], parts[1], stol(parts[2]), stol(parts[3]),
                                                     parts[4], parts[5], parts[6]);
             output.push_back(*newEvent);
         }
-        parts.clear(); // clean up parts for next line
+        parts.clear(); // Clean up parts for next line
     }
     return output;
 }
@@ -213,21 +183,22 @@ vector<Event> Event_Manager::get_public_events(){
     vector<Event> output;
     vector<string> parts;
     string line;
-    ifstream event_file;
-    event_file.open(STORAGE_FILE_PATH);
+
+    ifstream event_file(STORAGE_FILE_PATH);
+    if(!event_file.is_open())
+        return output; // Error occoured
     while(getline(event_file, line)){
         // Parse string into componants
         stringstream ss (line);
         while(getline(ss, line, "^@^"))
             parts.push_back(line);
-        // if access if public add to collection
+        // If access if public add to collection
         if (parts[4].compare("public") == 0){
-            Event_Factory* factory = new Event_Factory();
             Event* newEvent = factory->create_event(parts[0], parts[1], stol(parts[2]), stol(parts[3]),
                                                     parts[4], parts[5], parts[6]);
             output.push_back(*newEvent);
         }
-        parts.clear(); // clean up parts for next line
+        parts.clear(); // Clean up parts for next line
     }
     return output;
 }
@@ -236,7 +207,7 @@ vector<Event> Event_Manager::get_public_events(){
  * @brief       get a text rep for a given event
  *
  * @author      Vladimir Zhurov
- * @date        25/11/2019
+ * @date        30/11/2019
  * @param       event          The event
  * @return      txt_rep        A text representation of a given event object
  */
